1d23176ecaa6042dd075a84f09763f593970f449
Phloot <71058035+Phloot@users.noreply.github.com>
2022-07-16 22:11:22 -0600

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,27 +1,19 @@
 # src/functions.py
 import os
 import json
-<<<<<<< HEAD
 import logging
 import hashlib
 
-logger = logging.getLogger("oasis_bot_logger")
+logger = logging.getLogger("omen_bot_logger")
 
 # Reusable function to pull in config
 def return_config():
     base_dir = os.path.dirname(__file__)
     config_path = os.path.join(base_dir, 'configs', 'config.json')
-=======
-
-def return_config():
-    base_dir = os.path.dirname(__file__)
-    config_path = os.path.join(base_dir, 'configs/config.json')
->>>>>>> 0d10d75befd576c4faf79e842aaad44b4476add9
 
     config_json_file = open(config_path,)
     config = json.load(config_json_file)
     config_json_file.close()
-<<<<<<< HEAD
 
     return config
 
@@ -35,7 +27,3 @@ def md5_check(file_content):
     md5_hash.update(file_content)
     digest = md5_hash.hexdigest()
     #TODO: Add in comp against known checksums
-=======
-    
-    return config
->>>>>>> 0d10d75befd576c4faf79e842aaad44b4476add9
