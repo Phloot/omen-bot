6370c7936c6e5d851f3318b24745194506303dc2
Phloot <71058035+Phloot@users.noreply.github.com>
2022-07-16 22:12:09 -0600

    Update gw2_api.py

--- src/api/gw2_api.py
+++ src/api/gw2_api.py
@@ -13,8 +13,7 @@ of updating in the event an endpoint changes.
 class GW2Wrapper(object):
     def __init__(self):
         self.configs = return_config()
-<<<<<<< HEAD
-        self.logger = logging.getLogger("oasis_bot_logger")
+        self.logger = logging.getLogger("omen_bot_logger")
 
     # Internal URL builder function
     def _url_builder(self, endpoint, *args, **kwargs):
@@ -24,15 +23,6 @@ class GW2Wrapper(object):
             self.configs['gw2_api_config']['api_version'],
             endpoint,
             params
-=======
-
-    # Internal URL builder function
-    def _url_builder(self, endpoint):
-        return "{0}/{1}/{2}".format(
-            self.configs['gw2_api_config']['api_base_path'], 
-            self.configs['gw2_api_config']['api_version'],
-            endpoint
->>>>>>> 0d10d75befd576c4faf79e842aaad44b4476add9
             ) 
 
     # Internal general request function
@@ -41,11 +31,7 @@ class GW2Wrapper(object):
         return response.json()
 
     def worlds(self):
-<<<<<<< HEAD
         return self._api_request(self._url_builder(self.configs['gw2_endpoints']['worlds'], addl_params="?ids=all"))
-=======
-        return self._api_request(self._url_builder(self.configs['gw2_endpoints']['worlds']))
->>>>>>> 0d10d75befd576c4faf79e842aaad44b4476add9
 
     def wvw_matches(self):
         pass
