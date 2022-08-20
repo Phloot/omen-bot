4d7e9e95990f88216bf75345b360412c7a1164d8
Phloot <71058035+Phloot@users.noreply.github.com>
2022-08-20 15:24:11 -0600

    Add info command + required intents change (#21)
    * readme change
    
    * Readme typo fix
    
    * docker add
    
    * reorganize subdir
    
    * Delete config.json
    
    * gitignore changes
    
    * adding dockerfile changes
    
    * remove discord bot token from config+add args+dockerfile change
    
    * add readme
    
    * add dockerci workflow
    
    * docker build fix
    
    * added additional info to readme
    
    * added additional info to readme
    
    * added additional info to readme
    
    * added additional info to readme
    
    * added additional info to readme
    
    * added additional info to readme
    
    * readme changes
    
    * readme changes
    
    * remove print
    
    * Upgrade to discord.py:2.0.0 and compatibility changes
    
    * format() -> f strings
    
    * reqs and readme update
    
    * move hosted images to local
    
    * move to local images
    
    * info command
    
    * required intent

--- src/omen/cogs/bot_management.py
+++ src/omen/cogs/bot_management.py
@@ -3,12 +3,17 @@ import os
 import sys
 import discord
 import logging
+import socket
 import platform
+import datetime
+from datetime import datetime, timedelta
+from functions import convert_timedelta, return_config, attach_image
 from discord.ext import commands
 
 class BotManagement(commands.Cog):
     def __init__(self, omen_bot):
         self.omen_bot = omen_bot
+        self.configs = return_config()
         self.logger = logging.getLogger("omen_bot_logger")
 
     @commands.command()
@@ -25,9 +30,41 @@ class BotManagement(commands.Cog):
 
         await ctx.channel.send(f"Reloaded {cog_counter} cog(s)")
 
-    #@commands.command()
-    #@commands.is_owner()
-    #async def sysinfo(self, ctx, *, member: discord.Member = None):
+    @commands.command()
+    async def info(self, ctx, *, member: discord.Member = None):
+        author_thumb = "bot_icon.png"
+
+        try:
+            info={}
+            info['platform'] = platform.system() # Linux
+            info['platform-release'] = platform.release() # Version
+            info['architecture'] = platform.machine() # Architecture
+            info['hostname'] = socket.gethostname() # Hostname
+            info['processor'] = platform.processor() # Processor
+        except Exception as sysinfo_ex:
+            self.logger.warning(f"Error retrieving system info: {sysinfo_ex}")
+
+        info['ping'] = f'{round(self.omen_bot.latency * 1000)}ms' # Ping
+
+        # Generate uptime
+        time_diff = datetime.now() - self.omen_bot.start_time
+        hours, minutes, seconds = convert_timedelta(time_diff)
+        info['uptime'] = f"{time_diff.days}d {int(hours)}h {int(minutes)}m"
+        
+        # Fetch version
+        info['version'] = self.configs['internal']['version']
+
+        # Build and send embed
+        author_img = attach_image(author_thumb)
+        embed=discord.Embed(title="Bot Info", color=0xf0f0f0)
+        embed.set_author(name=f"Omen Bot {info['version']}", url="https://github.com/Phloot/omen-bot", icon_url=f"attachment://{author_thumb}")
+        embed.add_field(name="Uptime", value=f"{info['uptime']}")
+        embed.add_field(name="Ping", value=f"{info['ping']}")
+        embed.add_field(name="Hostname", value=f"{info['hostname']}")
+        embed.add_field(name="Platform", value=f"{info['platform']} {info['platform-release']}")
+        embed.add_field(name="Architecture", value=f"{info['architecture']}")
+        embed.add_field(name="Processor", value=f"{info['processor']}")
+        await ctx.channel.send(files=[author_img], embed=embed)
 
 async def setup(omen_bot):
     await omen_bot.add_cog(BotManagement(omen_bot))
