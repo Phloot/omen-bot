cbee3bd5f1faf6c2df69d02fa27a6847b16b76c7
Phloot <jay.a.ritter@gmail.com>
2022-12-26 18:04:09 -0700

    Add a couple of new API endpoints

--- src/omen/api/gw2_api.py
+++ src/omen/api/gw2_api.py
@@ -22,8 +22,10 @@ class GW2Wrapper(object):
 
     # Internal general request function
     def _api_request(self, url):
-        response = session.get(url)
-        return response.json()
+        return session.get(url).json()
+
+    def account(self):
+        return self._api_request(self._url_builder(self.configs['gw2_endpoints']['account']))
 
     def worlds(self):
         return self._api_request(self._url_builder(self.configs['gw2_endpoints']['worlds'], addl_params="?ids=all"))
